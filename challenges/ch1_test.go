d625cd76a59052bcf3988cb20f9286d7ada30a54
Jonathan Werrett <jonathan@werrett.co>
2017-04-02 11:56:40 -0700

    fixes broken ch1 test

--- challenges/ch1_test.go
+++ challenges/ch1_test.go
@@ -8,7 +8,7 @@ var ch1Tests = []struct {
   input    string
 	expected string 
 }{
-  {"49276d206b69c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"},
+  {"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"},
 }
 
 func Test_ch1Solution(t *testing.T) {
